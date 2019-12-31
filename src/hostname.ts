463c83be8a73aeb9131e2b29988efdc68569db8c
Chris Langager <christopher.langager@gmail.com>
2019-12-30 17:16:15 -0700

    undo hostname change

--- src/hostname.ts
+++ src/hostname.ts
@@ -1,2 +1,2 @@
-export const hostName = `http://localhost:3000`;
-// export const hostName = `https://nervous-minsky-d05cd8.netlify.com`;
+// export const hostName = `http://localhost:3000`;
+export const hostName = `https://nervous-minsky-d05cd8.netlify.com`;
