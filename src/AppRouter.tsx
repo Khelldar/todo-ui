c17c46a7c05170ef1e7f771bc4d3a4f8cba48238
Chris Langager <christopher.langager@gmail.com>
2019-12-30 17:14:31 -0700

    forgot some files

---
+++ src/AppRouter.tsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { TodosPage } from './pages/todos';
+import { LoginPage } from './pages/login';
+import { GoogleOauth } from './google';
+import { UserContext } from './userContext';
+
+export const AppRouter: React.FC = () => {
+  const [state] = useContext(UserContext);
+  return (
+    <Router>
+      <div>
+        <Switch>
+          {/* not sure how much sense this makes, but the "index" 
+          page changes based on whether the user is logged in */}
+          {state.state === 'loggedIn' && (
+            <Route path="/">
+              <TodosPage />
+            </Route>
+          )}
+
+          <Route path="/oauth/google">
+            <GoogleOauth />
+          </Route>
+          <Route path="/">
+            <LoginPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
---
+++ src/userReducer.ts
@@ -0,0 +1,55 @@
+import { User } from './generated/sdk';
+import { setToken, removeTokenFromStorage } from './token';
+
+export interface LoggedIn {
+  type: 'LoggedIn';
+  payload: {
+    user: User;
+    token: string;
+  };
+}
+
+//this event fires when the user comes back to the app and we find a token in local storage
+export interface Resumed {
+  type: 'Resumed';
+  payload: {
+    user: User;
+    token: string;
+  };
+}
+
+export interface LoggedOut {
+  type: 'LoggedOut';
+}
+
+export type Action = LoggedIn | Resumed | LoggedOut;
+
+export interface State {
+  state: 'init' | 'notLoggedIn' | 'loggedIn';
+  user?: User;
+  token?: string;
+}
+
+export const userReducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'LoggedIn':
+      //setting state outside of react feels pretty wrong, but I want to try it
+      setToken(action.payload.token);
+      return {
+        state: 'loggedIn',
+        user: action.payload.user,
+        token: action.payload.token,
+      };
+    case 'Resumed':
+      return {
+        state: 'loggedIn',
+        user: action.payload.user,
+        token: action.payload.token,
+      };
+    case 'LoggedOut':
+      removeTokenFromStorage();
+      return { state: 'notLoggedIn' };
+    default:
+      return state;
+  }
+};
