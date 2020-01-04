6c5cfd23ba1e13df4fc2016e816190de09b0c4d7
Chris Langager <christopher.langager@gmail.com>
2020-01-03 21:14:11 -0700

    tryng out RP

--- src/graphql/sdk.ts
+++ src/graphql/sdk.ts
@@ -2,10 +2,11 @@ import { GraphQLClient } from 'graphql-request';
 import { getSdk } from '../generated/sdk';
 
 // const url = 'http://localhost:5000/graphql';
-const url = '/graphql';
+const url = 'https://todo-server-gql.herokuapp.com/graphql';
 
 const client = new GraphQLClient(url, {
   credentials: 'include',
+  mode: 'cors',
 });
 
 export const sdk = getSdk(client);
