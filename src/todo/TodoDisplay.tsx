5fb5448c734fdc825ef89331402c05b6ee4d2ec0
Chris Langager <christopher.langager@gmail.com>
2020-01-20 12:03:31 -0700

    learning material ui

--- src/todo/TodoDisplay.tsx
+++ src/todo/TodoDisplay.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Paper, Grid, makeStyles } from '@material-ui/core';
+import Close from '@material-ui/icons/Close';
 
 export interface Todo {
   id: string;
@@ -11,7 +13,25 @@ export interface Props {
 }
 
 export const TodoDisplay: React.FC<Props> = props => {
+  const classes = useStyles();
   const { text } = props.todo;
 
-  return <div>{text}</div>;
+  return (
+    <Paper className={classes.paper}>
+      <Grid container>
+        <Grid item xs={11}>
+          {text}
+        </Grid>
+        <Grid item xs={1}>
+          <Close />
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 };
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: theme.spacing(2),
+  },
+}));
--- src/todo/TodoList.tsx
+++ src/todo/TodoList.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Todo, TodoDisplay } from './TodoDisplay';
+import { Grid, makeStyles } from '@material-ui/core';
 
 export interface Props {
   todos: Todo[];
 }
 
 export const TodoList: React.FC<Props> = props => {
+  const classes = useStyles();
   const { todos } = props;
   return (
-    <ul>
-      {todos.map(todo => (
-        <TodoDisplay key={todo.id} todo={todo}></TodoDisplay>
-      ))}
-    </ul>
+    <div className={classes.root}>
+      <Grid container spacing={2}>
+        {todos.map(todo => (
+          <Grid item xs={12}>
+            <TodoDisplay key={todo.id} todo={todo}></TodoDisplay>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
   );
 };
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}));
